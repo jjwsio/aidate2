3c1bf80786a8781db8974ff49212a8b172593cce
jjwsio <jandjdev20@gmail.com>
2024-09-30 21:47:54 +0100

    Update Dashboard.js

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -6,10 +6,9 @@ function Dashboard() {
 
   return (
     <div className="bg-gray-900 min-h-screen flex items-center justify-center">
-      <h1 className="text-3xl text-white">Welcome, {username}</h1>
+      <h1 className="text-3xl text-white">Welcome to your dashboard, {username}</h1>
     </div>
   );
 }
 
 export default Dashboard;
- 
